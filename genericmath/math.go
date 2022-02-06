f7bbbb98c73764391cf43cbffdb4dab48303cab2
Chris Smith <chris.smith@ahead.com>
2022-02-06 09:21:24 -0500

    Add Fuzzing testcase for ints

--- genericmath/math.go
+++ genericmath/math.go
@@ -1,9 +1,10 @@
 package genericmath
 
 type Number interface {
-	int | int32 | int64 | float32 | float64
+	int | int32 | int64 |float32 | float64
 }
 
+//Adds two numbers of the same type
 func Add[T Number](a T, b T) T {
 	return (a + b)
 }
--- genericmath/math_test.go
+++ genericmath/math_test.go
@@ -3,7 +3,6 @@ package genericmath
 import "testing"
 
 func TestAddInt(t *testing.T) {
-
 	ans := Add(int(3), int(5))
 
 	if ans != 8 {
@@ -18,4 +17,19 @@ func TestAddFloat64(t *testing.T) {
 	if ans != 4.1 {
 		t.Fail()
 	}
+}
+
+func FuzzAddInt(f *testing.F) {
+	testcases := []int{1, 2, 3, 4, 5, 67, 1231}
+
+	for _, tc := range testcases {
+		f.Add(tc, tc)
+	}
+
+	f.Fuzz(func(t *testing.T, a, b int) {
+		ans := a + b
+		if Add(a, b) != ans {
+			t.Errorf("A: %v + B: %v should be equal to: %v", a, b, ans)
+		}
+	})
 }
